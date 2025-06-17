dfcca3431babee93ef4779fd7b1b68e52d59ccd3
eldho1markose <157269287+eldho1markose@users.noreply.github.com>
2025-06-17 10:58:12 +0530

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -15,5 +15,7 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
+COPY wait-for-it.sh /wait-for-it.sh
+RUN chmod +x /wait-for-it.sh
 
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL="postgresql://user:pass@db:5432/productdb"
+SQLALCHEMY_DATABASE_URL="postgresql://user:pass@db:5432/product_service_db"
 
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
 Base = declarative_base()
